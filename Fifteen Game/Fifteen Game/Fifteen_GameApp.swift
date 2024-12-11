aaae7590717a1a67704064b77b0e66dc2b95df0c
Dias <dias.atudinov@gmail.com>
2024-12-11 13:36:17 +0500

    New files

---
+++ Fifteen Game/Fifteen Game/Fifteen_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fifteen_GameApp.swift
+//  Fifteen Game
+//
+//  Created by Dias Atudinov on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fifteen_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
