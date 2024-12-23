72ab9cf4f84b0f0b8582ecb613a67c2cafec880d
Dias <dias.atudinov@gmail.com>
2024-12-23 16:31:34 +0500

    Team View

--- Fifteen Game/Fifteen Game/Moduls/Teams/Team.swift
+++ Fifteen Game/Fifteen Game/Moduls/Teams/Team.swift
@@ -1,8 +1,15 @@
+//
+//  Team.swift
+//  Fifteen Game
+//
+//  Created by Dias Atudinov on 23.12.2024.
+//
+
+
 import Foundation
 
 struct Team : Identifiable, Equatable, Codable, Hashable {
     let id = UUID()
     let icon: String
     let name: String
-    var score: Int
 }
--- Fifteen Game/Fifteen Game/Moduls/Teams/TeamViewModel.swift
+++ Fifteen Game/Fifteen Game/Moduls/Teams/TeamViewModel.swift
@@ -1,37 +1,33 @@
+//
+//  TeamViewModel.swift
+//  Fifteen Game
+//
+//  Created by Dias Atudinov on 23.12.2024.
+//
+
+
 import SwiftUI
 
 class TeamViewModel: ObservableObject {
     @Published var teams: [Team] = [
-        Team(icon: "team1Logo", name: "Team Isleta", score: 0),
-        Team(icon: "team2Logo", name: "Vectors Team", score: 0),
-        Team(icon: "team3Logo", name: "Glory Team", score: 0)
+        Team(icon: "team1Logo1", name: "Tulalip Team"),
+        Team(icon: "team1Logo2", name: "Avrora Team"),
+        Team(icon: "team1Logo3", name: "Arax Team"),
+        Team(icon: "team1Logo4", name: "Vendetta team"),
+        Team(icon: "team1Logo5", name: "Tereria Team")
         
-    ] {
-        didSet {
-            saveArray()
-        }
-    }
+    ]
     
-    @Published var currentTeam: Team? = Team(icon: "team1Logo", name: "Team Isleta", score: 0) {
+    @Published var currentTeam: Team? {
         didSet {
             saveTeam()
         }
     }
     
     init() {
-        
         loadTeam()
-        loadArray()
     }
     private let userDefaultsTeamKey = "currentTeam"
-    private let userDefaultsArrayKey = "teams"
-
-    
-    func addScore(points: Int) {
-        if let index = teams.firstIndex(where: { $0.name == currentTeam?.name }) {
-            teams[index].score += points
-        }
-    }
     
     func saveTeam() {
         if let currentTeam = currentTeam {
@@ -45,23 +41,6 @@ class TeamViewModel: ObservableObject {
         if let savedData = UserDefaults.standard.data(forKey: userDefaultsTeamKey),
            let loadedTeam = try? JSONDecoder().decode(Team.self, from: savedData) {
             currentTeam = loadedTeam
-        } else {
-            print("No saved data found")
-        }
-    }
-    
-    
-    func saveArray() {
-            if let encodedData = try? JSONEncoder().encode(teams) {
-                UserDefaults.standard.set(encodedData, forKey: userDefaultsArrayKey)
-            }
-        
-    }
-    
-    func loadArray() {
-        if let savedData = UserDefaults.standard.data(forKey: userDefaultsArrayKey),
-           let loadedArray = try? JSONDecoder().decode([Team].self, from: savedData) {
-            teams = loadedArray
         } else {
             print("No saved data found")
         }
